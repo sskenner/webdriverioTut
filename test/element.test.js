51dff79793807a3af7f55e732e742f2ec65092b7
skA <sskenner@gmail.com>
2019-11-10 21:44:04 -0500

    update for element interaction, part 2

--- test/element.test.js
+++ test/element.test.js
@@ -1,7 +1,11 @@
+const internetPage = require("../pages/internet.page")
+
 describe("Interacting with elements", function(){
   it("Get text for element", () => {
     browser.url('/')
     let text = $("//*[@id='page-footer']").getText()
     console.log(text)
+    internetPage.getLiText()
+    internetPage.getSpecifcElementText(3)
   })
 })
