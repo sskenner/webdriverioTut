a26fd5cd9fc2de04ce107886a13063f1b70b357f
skA <sskenner@gmail.com>
2019-11-11 07:44:02 -0500

    update for element actions

--- pages/internet.page.js
+++ pages/internet.page.js
@@ -5,6 +5,9 @@ class Internet {
   get subHeading() {
     return $("h2");
   }
+  get h3Header() {
+    return $("h3");
+  }
   get pageFooter() {
     return $("#page-footer");
   }
@@ -21,21 +24,61 @@ class Internet {
   get firstLink() {
     return $("ul li:nth-child(1) a");
   }
+  link(index) { return $(`ul li:nth-child(${index}) a`)}
+
+  checkboxes(index) { return $(`#checkboxes input:nth-child(${index})`) }
+
+  get username() { return $('#username') }
+  get password() { return $('#password') }
+
+  /**
+   * Enter the username into the field
+   * @param {String} text username to be entered
+   */
+  enterUsername(text) {
+    this.username.waitForDisplayed()
+    this.username.setValue(text)
+  }
+
+  /**
+   * Enter the password into the field
+   * @param {String} text password to be entered
+   */
+  enterPassword(text) {
+    this.password.waitForDisplayed()
+    this.password.setValue(text)
+  }
+
+  /**
+   * Clicks on the link based on the index provided
+   * @param {Number} index the index of the element
+   */
+  clickLink(index) {
+    this.link(index).waitForDisplayed()
+    this.link(index).click()
+  }
+
+  clickCheckbox(index) {
+    this.checkboxes(index).waitForDisplayed()
+    this.checkboxes(index).click()
+  }
+
   getLiText() {
     this.childElements.filter(element => {
       console.log(element.getText());
     });
   }
 
-  getSpecifcElementText(index) {
-    console.log(this.specificChildElement(index).getText());
+  getSpecificElementText(index) {
+    this.specificChildElement(index).waitForDisplayed()
+    return this.specificChildElement(index).getText();
   }
 
   clickOnLink() {
     if(this.firstLink.isDisplayed() === true) {
       this.firstLink.click()
     }
-    browser.pause(5000)
+    this.h3Header.waitForDisplayed()
   }
 }
 module.exports = new Internet();
