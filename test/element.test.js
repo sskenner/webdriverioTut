d778835e25a879902c3f96e480fc7e9b1d9a2395
skA <sskenner@gmail.com>
2019-11-10 22:40:08 -0500

    update for element state..1

--- test/element.test.js
+++ test/element.test.js
@@ -7,5 +7,20 @@ describe("Interacting with elements", function(){
     console.log(text)
     internetPage.getLiText()
     internetPage.getSpecifcElementText(3)
-  })
+  });
+  it('Is footer displayed?', () => {
+    console.log(internetPage.pageFooter.isDisplayed())
+  });
+  it('Does the header exist?', () => {
+    console.log(internetPage.pageHeader.isExisting())
+  });
+  it('Is footer in viewport?', () => {
+    console.log(internetPage.pageFooter.isDisplayedInViewport())
+  });
+  it('Is header in viewport?', () => {
+    console.log(internetPage.pageHeader.isDisplayedInViewport())
+  });
+  it('Is subheader enabled?', () => {
+    console.log(internetPage.subHeading.isEnabled())
+  });
 })
